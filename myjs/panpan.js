ad4c4e17df00c37b667fd0ac76f392802a10cb5b
Panpan-GitHub <574155661@qq.com>
2018-12-21 21:45:16 +0800

    2018年12月21日 21:45:13

--- myjs/panpan.js
+++ myjs/panpan.js
@@ -1,86 +1,214 @@
+var id = 1;
+/*
+判断登录用户
+*/
+if (id != 1) {
+    let alist = document.getElementsByClassName("display");
+    for (let idx = 0; idx < alist.length; idx++) {
+        var mya = alist[idx];
+        mya.style.display = "none";
+    }
+    var n = $('.dad-draggable').dad();
+    n.deactivate();
+}
+
+
+
+
+
+
+
+
+
+/*
+添加模块
+*/
+
+// 图片管理模块
+// 打开模态框按钮
+$("#piclist").click(function () {
+    imgmanage.getAll()
+    // $("select.image-picker").imagepicker({
+    //     hide_select: false
+    // });
+});
+// 上传图片按钮
+$("#upbtn").click(function () {
+    imgmanage.upimg();
+    imgmanage.getAll();
+});
+
+const imgmanage = {
+    "getAll": function () {
+        $("#mypic").html("");
+
+        $.ajax({
+            url: 'http://192.168.1.104:8080/photo/getAll',
+            type: 'POST',
+            data: {
+                userId: id
+            },
+            dataType: "JSON",
+            success: function (data) {
+                $.each(eval(data).obj, function (i, item) {
+                    $("#mypic").append(
+                        `<option class="maxwidth-80" data-img-src="` + item.photoPath + `"value='` + item.id + `'>` + item.photoPath + `</option>`);
+                    // $("ul.image_picker_selector").append(
+                    //     `  <li><div class="thumbnail"><img class="image_picker_image" src="` + item.photoPath + `"></div></li>`
+                    // )
+                })
+                $("select.image-picker").imagepicker({
+                    hide_select: false
+                });
+            },
+            error: function (e) {
+                console.log(e)
+
+            }
+        });
+
+    },
+
+    "upimg": function () {
+        let formdata = new FormData($("#form-upimg")[0]);
+        // photo.append("userId", 1);
+        // photo.append("photoPath", 1);
+        $.ajax({
+            url: 'http://192.168.1.104:8080/photo/add',
+            type: 'POST',
+            data: formdata,
+            processData: false,
+            contentType: false,
+            success: function () {},
+            error: function () {}
+        })
+    },
+
+}
+
 // 列表多图
-// 获取所有图片
-$("#imglist").click(function () {
-    $.ajax({
-        url:'',
-        type:'',
-        success:function (data) {
-            $.each(data,function (i,item) {
-                $("#mypic").append( 
-               `<option class="maxwidth-80" data-img-src='`+ item.img +`' value='1'>`+ item.src +`</option>`)
-            })
-        },
-        error:function (e) {
 
-        }
-});
+$("#useimg").click(function () {
+    imgList.getId();
+    imgList.add();
+    imgList.addTxt();
 });
 
-$("#useimg").click(addImgList());
-function addImgList() {
-    const imgList = {
-        "title": 0,
-        "img": $("#mypic option:selected"),
-        "data": 0,
-        "content": 0,
-        "txt":"<!-- imglist -->",
-        "txt1": ` <div class="col-md-12">
+const imgList = {
+    "modId": 0,
+    "title": 0,
+    "txt": "<!-- imglist -->",
+    "txt-one": ` <div class="col-md-12">
         <div class="col-md-6 col-md-offset-3">
             <div class="title-section">
-                <h2 class="title"><span class="typo">&amp;</span>`+title+`</h2>
+                <h2 class="title"><span class="typo">&amp;</span>` + this.title + `</h2>
             </div>
         </div>
         <div class="col-md-12">
         <div class="blog-carousel-slider post-wrap">
-        ` ,
-
-        "txt2": `</div>
+        `,
+    "txt-two": `</div>
         </div>
-    </div>`
-    ,
-       
-        "add": function () {
-            $.ajax({
-                url:'',
-                type:'',
-                success:function (data) {
-                    $.each(data,function (i,item) {
-                        this.txt+=`   
+    </div>`,
+    //获取id
+    "getId": function () {
+        this.title = $("#imglist-name").val();
+        console.log(this.title)
+        $.ajax({
+            url: 'http://192.168.1.104:8080/module/add',
+            type: 'POST',
+            data: {
+                userId: id,
+                name: this.title,
+                type: "列表多图"
+            },
+            success: function (data) {
+                this.modId = eval(data).obj.id;
+            }
+        })
+    },
+    "add": function () {
+        let arr = new Array();
+        let img = new Array();
+        let txt3 = "";
+
+        $("#mypic").find("option:selected").each(function () {
+            img.push($(this).text());
+        })
+        console.log(img)
+        for(let i=0;i<img.length;i++){
+            arr.push(
+               {
+                  "moduleId":this.modId,
+                  "photoPath":img[i],
+                  "description":"Default introduction",
+                  "linkPath":"#"
+                }
+            )
+        }
+        console.log(arr)
+        $.ajax({
+            type: 'POST',
+            async: false,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            url: 'http://192.168.1.104:8080/multiPhoto/save?moduleId=6',
+            data: JSON.stringify(arr),
+            dataType: 'json',
+            success: function (data) {
+                $.each(img, function (i, item) {
+                    let text = `   
                         <article class="entry object">
                             <div class="feature-post ">
                                 <a class="img-post" href="blog-single.html">
-                                    <img src="images/blog/11.jpg" alt="image">
+                                    <img src="` + item + `" alt="image">
                                 </a>
                             </div><!-- /.feature-post -->
                             <div class="main-post">
                                 <h3 class="entry-title"><a href="blog-single.html">Fresh and Cool This
                                         Summer</a></h3>
-                                <div class="entry-meta">
-                                    <span class="entry-time">April 24, 2017</span>
-                                </div>
-                                <div class="entry-content">Did you know that manufacturers recommend
-                                    checking
-                                    your Climate Control Air Conditioning systems every two years to help
-                                    keep
-                                    your...
-                                </div>
                             </div><!-- /.main-post -->
                         </article><!-- /.entry -->
-        `
-                    });
-                },
-                error:function (e) {
+        `;
+        txt3 = txt3.concat(text);
+        this.txt = txt3;
+        console.log(this.txt)
+                });
+            },
+            error: function (e) {
 
-                }
-            });
-            // 遍历内容
-            $("#imglist").append(
+            }
+        });
+    },
+    // 组合内容
+    "addTxt": function () {
 
-            )
-        },
-        // 组合内容
-        "addTxt" : function () {
-            $("#bg-contant").append(txt1+txt);
-        },
-    };
+        $("#bg-contant").append(this["txt-one"] + this.txt + this["txt-two"]);
+    },
+};
+
+//添加文章列表
+const addText = {
+    "getId": function () {
+        $.ajax({
+            url: 'http://192.168.1.104:8080/module/add',
+            type: 'POST',
+            data: {
+                userId: id,
+                name: this.title,
+                type: "multiPhoto"
+            },
+            success: function (data) {
+                this.modId = eval(data).obj.id;
+            }
+        })
+    },
 }
+
+
+
+
+/*
+获得模块
+*/
---
+++ myjs/zujian.js
@@ -0,0 +1,126 @@
+$(document).ready(function () {
+
+    //瀑布流布局
+    var container = $("select.image-picker.masonry").next("ul.thumbnails");
+
+    container.imagesLoaded(function () {
+        container.masonry({
+            itemSelector: "li"
+        });
+    });
+
+    $("#firstpane .menu_body:eq(0)").show();
+    $("#firstpane p.menu_head").click(function(){
+        $(this).addClass("current").next("div.menu_body").slideToggle(300).siblings("div.menu_body").slideUp("slow");
+        $(this).siblings().removeClass("current");
+    });
+    $("#secondpane .menu_body:eq(0)").show();
+    $("#secondpane p.menu_head").mouseover(function(){
+        $(this).addClass("current").next("div.menu_body").slideDown(500).siblings("div.menu_body").slideUp("slow");
+        $(this).siblings().removeClass("current");
+    });
+});
+
+$(function () {
+
+    //dad simple demo
+    $('.dad').dad();
+
+    //dad dropzone
+    var d = $('.dad2').dad();
+    d.addDropzone('.dropzone', function (e) {
+        e.remove();
+    });
+
+    //dad activate and deactivate
+    $('.act').on('click', function () {
+        $('.btndemo').not(this).removeClass('active');
+        $(this).addClass('active');
+        d.css('opacity', 1);
+        d.activate();
+    });
+
+    $('.deact').on('click', function () {
+        $('.btndemo').not(this).removeClass('active');
+        $(this).addClass('active');
+        d.css('opacity', 0.5);
+        d.deactivate();
+    });
+
+    $('.dad-draggable').dad({
+        draggable: '.draggable'
+    });
+
+
+
+    $("select.image-picker").imagepicker({
+        hide_select: false
+    });
+
+    $("select.image-picker.show-labels").imagepicker({
+        hide_select: false,
+        show_label: true
+    });
+
+    $("select.image-picker.limit_callback").imagepicker({
+        limit_reached: function () {
+            alert('We are full!')
+        },
+        hide_select: false
+    });
+
+    //瀑布流布局
+    var container = $("select.image-picker.masonry").next("ul.thumbnails");
+
+    container.imagesLoaded(function () {
+        container.masonry({
+            itemSelector: "li"
+        });
+    });
+
+});
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text('New message to ' + recipient)
+    modal.find('.modal-body input').val(recipient)
+})
+
+$('#toTop').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 1000);
+
+});
+
+
+$("#file-1").fileinput({
+    uploadUrl: '#', // you must set a valid URL here else you will get an error
+    allowedFileExtensions: ['txt', 'doc', 'docx'],
+    overwriteInitial: false,
+    maxFileSize: 1000,
+    maxFilesNum: 10,
+    slugCallback: function (filename) {
+        return filename.replace('(', '_').replace(']', '_');
+    }
+});
+
+$("#file-2").fileinput({
+    uploadUrl: '#', // you must set a valid URL here else you will get an error
+    overwriteInitial: false,
+    maxFileSize: 1000,
+    maxFilesNum: 10,
+    slugCallback: function (filename) {
+        return filename.replace('(', '_').replace(']', '_');
+    }
+});
+
+var E = window.wangEditor
+var editor = new E('#editor')
+// 或者 var editor = new E( document.getElementById('editor') )
+editor.create();
+
